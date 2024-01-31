1ebc174376dd9047632189c0edbcb6bd0e57942f
bholap1506 <avinashrajpatel1506@gmail.com>
2024-01-31 11:30:27 +0530

    Raja Ji

---
+++ Arithmatic.java
@@ -0,0 +1,25 @@
+public class Arithmatic {
+    public static void main(String arg[])
+    {
+        int n1 =20;
+        int n2 =6;
+        int add = n1+n2;
+        int sub = n1-n2;
+        int mul = n1*n2;
+        int div = n1/n2;
+        int mod = n1%n2;
+        System.out.println("First Number : "+n1);
+        System.out.println("Second Number : "+n2);
+        System.out.println("Add "+add);
+        System.out.println("Sub "+sub);
+        System.out.println("Mul "+mul);
+        System.out.println("Div "+div);
+        System.out.println("Mod "+mod);
+        System.out.println("Add "+add);
+        System.out.println("Sub "+sub);
+        System.out.println("Mul "+mul);
+        System.out.println("Div "+div);
+        System.out.println("Mod "+mod);
+    }
+
+}
---
+++ Assignment.java
@@ -0,0 +1,12 @@
+public class Assignment {
+    public static void main(String args[])
+    {
+        int a = 10;
+        float b = a;
+        System.out.println(b);
+
+        int c = 10;
+        c -=5;
+        System.out.println(c);
+    }
+}
